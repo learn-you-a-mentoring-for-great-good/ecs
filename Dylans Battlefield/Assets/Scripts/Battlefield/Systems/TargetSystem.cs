7439b306b291a0a06bd9656230fca345db9f05ad
DylanCassidy <the.dylan.cassidy@gmail.com>
2019-05-10 17:04:36 -0600

    improved the random targeting

--- Dylans Battlefield/Assets/Scripts/Battlefield/Systems/TargetSystem.cs
+++ Dylans Battlefield/Assets/Scripts/Battlefield/Systems/TargetSystem.cs
@@ -10,27 +10,51 @@ namespace Battlefield
 {
     public class TargetSystem : JobComponentSystem
     {
-        struct TargetJob : IJobForEach<Target, Translation, Rotation>
+        Unity.Mathematics.Random _rng = new Unity.Mathematics.Random(1u);
+
+        struct TargetJob : IJobForEachWithEntity<Target, Translation, Rotation>
         {
-            public void Execute(ref Target target, [ReadOnly] ref Translation position, [ReadOnly] ref Rotation rotation)
+            static readonly float3 MIN = new float3(-10f, 0f, -10f);
+            static readonly float3 MAX = new float3(10f, 0f, 10f);
+
+            public uint baseSeed;
+            public float frameCount;
+
+            public void Execute(Entity entity, int index, ref Target target, [ReadOnly] ref Translation position, [ReadOnly] ref Rotation rotation)
             {
                 // TODO: set target
                 if (true)
                 {
-                    target.Value = new float3();
+                    if (frameCount % 100f == 0f)
+                    {
+                        // TODO: make the index change the seed more dramaticly
+                        Unity.Mathematics.Random rng = new Unity.Mathematics.Random(baseSeed + (uint)index);
+
+                        float3 _randomTarget = rng.NextFloat3(MIN, MAX);
+                        _randomTarget.y = 0f;
+
+                        target.Value = _randomTarget;
+                    }
                 }
                 else
                 {
-                    target.Value.y = -2f;
+                    //target.Value.y = -2f;
                 }
             }
         }
 
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
+            if (Time.frameCount % 100 == 0)
+            {
+                // I need to call this here to mutate the rng state, because the threads dont?
+                _rng.NextFloat3();
+            }
+
             TargetJob targetJob = new TargetJob
             {
-
+                baseSeed = _rng.state,
+                frameCount = Time.frameCount
             };
 
             return targetJob.Schedule(this, inputDeps);
